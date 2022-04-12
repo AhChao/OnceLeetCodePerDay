c63686f1f069b69b0809a70be076dd192122c98e
stevne.chao <gash612@gmail.com>
2022-04-12 15:19:09 +0800

    [0412] 2235. Add Two Integers

---
+++ OnceLeetCodePerDay/Implements/L2235AddTwoIntegers.cs
@@ -0,0 +1,9 @@
+namespace OnceLeetCodePerDay.Implements;
+
+public class L2235AddTwoIntegers
+{
+    public int Sum(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+}
--- OnceLeetCodePerDay/Tests/L2235AddTwoIntegersTests.cs
+++ OnceLeetCodePerDay/Tests/L2235AddTwoIntegersTests.cs
@@ -1,13 +1,29 @@
 using NUnit.Framework;
+using OnceLeetCodePerDay.Implements;
 
 namespace OnceLeetCodePerDay.Tests
 {
+    //https://leetcode.com/problems/add-two-integers/
     public class L2235AddTwoIntegersTests
-    { 
-        [Test]
-        public void Test1()
+    {
+        private L2235AddTwoIntegers _l2235AddTwoIntegers;
+
+        [SetUp]
+        public void SetUp()
         {
-            Assert.Pass();
+            _l2235AddTwoIntegers = new L2235AddTwoIntegers();
+        }
+
+        [Test]
+        public void Input_12_5_Should_Return_17()
+        {
+            Assert.AreEqual(17, _l2235AddTwoIntegers.Sum(12, 5));
+        }
+
+        [Test]
+        public void Input_Negative_10_4_Should_Return_Negative_6()
+        {
+            Assert.AreEqual(-6, _l2235AddTwoIntegers.Sum(-10, 4));
         }
     }
 }
