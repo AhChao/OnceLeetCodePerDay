d92b60710b451cebbc666dd159b1dcfefd3d6d7f
stevne.chao <gash612@gmail.com>
2022-04-12 15:13:06 +0800

    Init setup

---
+++ OnceLeetCodePerDay/Tests/L2235AddTwoIntegersTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace OnceLeetCodePerDay.Tests
+{
+    public class L2235AddTwoIntegersTests
+    { 
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ OnceLeetCodePerDay/UnitTest1.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace OnceLeetCodePerDay
+{
+    //This repo refer list about https://docs.google.com/spreadsheets/d/1je6J87BX5C5fo5Gbok1TJncVK3-UFkiDznHUzhtHbVU/edit#gid=0
+    public class TestTemplate
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
