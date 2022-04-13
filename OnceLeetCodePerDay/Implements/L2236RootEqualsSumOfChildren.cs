ee45348c8faf602b6c82d16d76d86c4e2c47c6b5
stevne.chao <gash612@gmail.com>
2022-04-13 16:55:39 +0800

    [0413] 2236. Root Equals Sum of Children

---
+++ OnceLeetCodePerDay/Implements/L2236RootEqualsSumOfChildren.cs
@@ -0,0 +1,23 @@
+namespace OnceLeetCodePerDay.Implements;
+
+public class L2236RootEqualsSumOfChildren
+{
+    public bool CheckTree(TreeNode root)
+    {
+        return (root.left.val + root.right.val) == root.val;
+    }
+}
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ OnceLeetCodePerDay/Tests/L2236RootEqualsSumOfChildrenTests.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+using OnceLeetCodePerDay.Implements;
+
+namespace OnceLeetCodePerDay
+{
+    //This repo refer list about https://docs.google.com/spreadsheets/d/1je6J87BX5C5fo5Gbok1TJncVK3-UFkiDznHUzhtHbVU/edit#gid=0
+    public class L2236RootEqualsSumOfChildrenTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _l2236RootEqualsSumOfChildren = new L2236RootEqualsSumOfChildren();
+        }
+
+        private L2236RootEqualsSumOfChildren _l2236RootEqualsSumOfChildren;
+        private TreeNode _root;
+
+        [Test]
+        public void Input_10_4_6_Result_Should_Be_True()
+        {
+            _root = setupAThreeNodeBinaryTreeByInts(10, 4, 6);
+            OutputBoolShouldBe(true);
+        }
+
+        [Test]
+        public void Input_5_3_1_Result_Should_Be_False()
+        {
+            _root = setupAThreeNodeBinaryTreeByInts(5, 3, 1);
+            OutputBoolShouldBe(false);
+        }
+
+        private void OutputBoolShouldBe(bool expected)
+        {
+            Assert.AreEqual(expected, _l2236RootEqualsSumOfChildren.CheckTree(_root));
+        }
+
+        private static TreeNode setupAThreeNodeBinaryTreeByInts(int rootVal, int leftVal, int rightVal)
+        {
+            return new TreeNode(rootVal, new TreeNode(leftVal), new TreeNode(rightVal));
+        }
+    }
+}
