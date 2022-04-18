028ec35bcc36725de0337ce680962f4f8ab338aa
stevne.chao <gash612@gmail.com>
2022-04-19 00:01:39 +0800

    [0418_Add] Refactor the first test and add a test

--- OnceLeetCodePerDay/Tests/L1108DefangingAnIPAddressTests.cs
+++ OnceLeetCodePerDay/Tests/L1108DefangingAnIPAddressTests.cs
@@ -6,12 +6,32 @@ namespace OnceLeetCodePerDay
     //https://leetcode.com/problems/defanging-an-ip-address/
     public class L1108DefangingAnIPAddressTests
     {
+        [SetUp]
+        public void SetUp()
+        {
+            _l1108DefangingAnIpAddress = new L1108DefangingAnIPAddress();
+        }
+
+        private L1108DefangingAnIPAddress _l1108DefangingAnIpAddress;
+        private string _address;
+
         [Test]
         public void Dots_Of_1111_Should_Be_Replaced()
         {
-            var l1108DefangingAnIpAddress = new L1108DefangingAnIPAddress();
-            var address = "1.1.1.1";
-            Assert.AreEqual(l1108DefangingAnIpAddress.DefangIPaddr(address), "1[.]1[.]1[.]1");
+            _address = "1.1.1.1";
+            DefangResultShouldBe("1[.]1[.]1[.]1");
+        }
+
+        [Test]
+        public void Dots_Of_2551000500_Should_Be_Replaced()
+        {
+            _address = "255.100.50.0";
+            DefangResultShouldBe("255[.]100[.]50[.]0");
+        }
+
+        private void DefangResultShouldBe(string expected)
+        {
+            Assert.AreEqual(_l1108DefangingAnIpAddress.DefangIPaddr(_address), expected);
         }
     }
 }
