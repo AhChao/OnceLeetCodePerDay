b6c21bd3a41477c769ea2c2ed9607ea653dae62c
stevne.chao <gash612@gmail.com>
2022-05-01 01:09:03 +0800

    [0430][Refactor] Fill 6052 testcase and refactor it

--- OnceLeetCodePerDay/Contest/0430/Tests/L6052MinimumAverageDifferenceTests.cs
+++ OnceLeetCodePerDay/Contest/0430/Tests/L6052MinimumAverageDifferenceTests.cs
@@ -6,12 +6,32 @@ namespace OnceLeetCodePerDay
     //https://leetcode.com/contest/biweekly-contest-77/problems/minimum-average-difference/
     public class L6052MinimumAverageDifferenceTests
     {
+        [SetUp]
+        public void SetUp()
+        {
+            _l6052MinimumAverageDifference = new L6052MinimumAverageDifference();
+        }
+
+        private L6052MinimumAverageDifference _l6052MinimumAverageDifference;
+        private int[] _nums;
+
         [Test]
         public void Input_253953_Result_Should_Be_3()
         {
-            var l6052MinimumAverageDifference = new L6052MinimumAverageDifference();
-            var nums = new[] { 2, 5, 3, 9, 5, 3 };
-            Assert.AreEqual(3,l6052MinimumAverageDifference.MinimumAverageDifference(nums));
+            _nums = new[] {2, 5, 3, 9, 5, 3};
+            ResultShouldBe(3);
+        }
+
+        [Test]
+        public void Input_0_Result_Should_Be_0()
+        {
+            _nums = new[] {0};
+            ResultShouldBe(0);
+        }
+
+        private void ResultShouldBe(int expected)
+        {
+            Assert.AreEqual(expected, _l6052MinimumAverageDifference.MinimumAverageDifference(_nums));
         }
     }
 }
